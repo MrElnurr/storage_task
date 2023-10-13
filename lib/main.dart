25663ddb9dc83fffc2384189f77e96b8778f94c9
MrElnurr <elnurkerimli123@gmail.com>
2023-10-13 22:48:06 +0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,128 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase_storage/firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp()); //sizin classin adi nedirse o
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  PlatformFile? pickedFile;
+  UploadTask? uploadTask;
+
+  Future selectFile() async {
+    final result = await FilePicker.platform.pickFiles();
+    if (result == null) return;
+    setState(() {
+      pickedFile = result.files.first;
+    });
+  }
+
+  Future uploadFile() async {
+    final path = 'files/${pickedFile!.name}';
+    final file = File(pickedFile!.path!);
+    final ref = FirebaseStorage.instance.ref().child(path);
+    setState(() {
+      uploadTask = ref.putFile(file);
+    });
+    final snapshot = await uploadTask!.whenComplete(() {});
+    final urlDownload = await snapshot.ref.getDownloadURL();
+    if (kDebugMode) {
+      print('Download link: $urlDownload');
+    }
+    setState(() {
+      uploadTask = null;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              if (pickedFile != null)
+                Expanded(
+                    child: Container(
+                  color: Colors.blueAccent,
+                  child: Center(
+                    child: Image.file(
+                      File(pickedFile!.path!),
+                      width: double.infinity,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                )),
+              const SizedBox(
+                height: 30,
+              ),
+              ElevatedButton(
+                  onPressed: selectFile, child: const Text('Select file')),
+              const SizedBox(
+                height: 25,
+              ),
+              ElevatedButton(
+                onPressed: uploadFile,
+                child: const Text('Upload file'),
+              ),
+              buildProgress(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildProgress() => StreamBuilder<TaskSnapshot>(
+        stream: uploadTask?.snapshotEvents,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final data = snapshot.data!;
+            double progress = data.bytesTransferred / data.totalBytes;
+            return SizedBox(
+              height: 50,
+              child: Stack(
+                fit: StackFit.expand,
+                children: [
+                  LinearProgressIndicator(
+                    value: progress,
+                    backgroundColor: Colors.blueGrey,
+                    color: Colors.greenAccent,
+                  ),
+                  Center(
+                    child: Text(
+                      '${(100 * progress).roundToDouble()} %',
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          } else {
+            return const SizedBox(height: 50);
+          }
+        },
+      );
+}
